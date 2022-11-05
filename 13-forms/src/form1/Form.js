c254c29927ed4a6b58180167f2325e3de662a727
Cielo <macielo.tablate@gmail.com>
2022-11-05 14:16:12 +0100

    add checkbox & radio

--- 13-forms/src/form1/Form.js
+++ 13-forms/src/form1/Form.js
@@ -4,16 +4,20 @@ function Form() {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
-    email: ''
+    email: '',
+    message: '',
+    isFriendly: true,
+    employment: '',
   });
 
-  console.log(formData);
+  console.log(formData.employment);
 
   function handleChange(e) {
+    const {name, value, type, checked} = e.target;
     setFormData((prevFormData) => {
       return {
         ...prevFormData,
-        [e.target.name]: e.target.value,
+        [name]: type === 'checkbox' ? checked : value,
       };
     });
   }
@@ -24,6 +28,7 @@ function Form() {
         type='text'
         placeholder='First Name'
         name='firstName'
+        value={formData.firstName}
         onChange={handleChange}
       />
 
@@ -31,6 +36,7 @@ function Form() {
         type='text'
         placeholder='Last Name'
         name='lastName'
+        value={formData.lastName}
         onChange={handleChange}
       />
 
@@ -38,8 +44,65 @@ function Form() {
         type='email'
         placeholder='Email'
         name='email'
+        value={formData.email}
         onChange={handleChange}
       />
+
+      <textarea
+        cols='30'
+        rows='10'
+        id='message'
+        name='message'
+        placeholder='Message:'
+        value={formData.message}
+        onChange={handleChange}
+      ></textarea>
+
+      <label htmlFor='friendlyCheckbox'>
+        <input
+          type='checkbox'
+          id='friendlyCheckbox'
+          name='isFriendly'
+          checked={formData.isFriendly}
+          onChange={handleChange}
+        />
+        {'  '}
+        Are you friendly?
+      </label>
+
+      <fieldset>
+        <legend className='legend'>Current Employment Status</legend>
+        <input
+          type='radio'
+          id='unemployed'
+          name='employment'
+          value="unemployed"
+          onChange={handleChange}
+        />
+        {'  '}
+        <label htmlFor='unemployed'>Unemployed</label>
+        <br />
+        <input
+          type='radio'
+          id='part-time'
+          name='employment'
+          value="part-time"
+          onChange={handleChange}
+        />
+        {'  '}
+        <label htmlFor='part-time'>Part-time</label>
+        <br />
+        <input
+          type='radio'
+          id='full-time'
+          name='employment'
+          value="full-time"
+          onChange={handleChange}
+        />
+        {'  '}
+        <label htmlFor='full-time'>Full-time</label>
+        <br />
+      </fieldset>
     </form>
   );
 }
